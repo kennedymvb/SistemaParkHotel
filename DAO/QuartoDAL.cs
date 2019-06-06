f707efdd3ba80cb5592102b71417e3325b33e712
kennedymvb <kennedymnovas@gmail.com>
2019-06-06 14:31:16 -0300

    atualização

--- DAO/QuartoDAL.cs
+++ DAO/QuartoDAL.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAO
+namespace Metadata
 {
-    public class Class1
+    class Quarto
     {
     }
 }
---
+++ Metadata/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metadata
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+}
