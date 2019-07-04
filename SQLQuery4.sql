eb1886ab467d4442993b522ac46a5bb2fbba24ac
kennedy <kennedymnovas@gmail.com>
2019-07-03 23:22:39 -0300

    p

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,3 +1,1 @@
-﻿select * from USUARIOS
-
-insert into USUARIOS values ('kennedy','11111111',  '11111111100','rua bernardo reiter','47996207702', 'kennedy@gmail.com', '12345678', 0)
+﻿select * from CHECKins
